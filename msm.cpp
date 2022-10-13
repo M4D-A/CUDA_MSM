16e59723ed35d4b2d5ff90af46dbbce1b9eeec70
M4D-A <jagielski.adamm@gmail.com>
2022-10-13 16:39:54 +0200

    Modular and Montgomerry Multiplication - working, tested

--- msm.cpp
+++ msm.cpp
@@ -4,45 +4,62 @@
 #include <memory.h>
 #include <stdexcept>
 
-void print(uint32_t a[12]) {
-    for (int i = 11; i >=0; i--) {
-        printf("%08X ", a[i]);
-        if(i%4==0) printf("\n");
+uint64_t P[6] = {
+    0x8508c00000000001,
+    0x170b5d4430000000,
+    0x1ef3622fba094800,
+    0x1a22d9f300f5138f,
+    0xc63b05c06ca1493b,
+    0x01ae3a4617c510ea
+};
+
+uint64_t R[6] = {
+    0x7af73fffffffffff,
+    0xe8f4a2bbcfffffff,
+    0xe10c9dd045f6b7ff,
+    0xe5dd260cff0aec70,
+    0x39c4fa3f935eb6c4,
+    0x0051c5b9e83aef15
+};
+
+uint64_t Ri[6] = {
+    0x2b0909a28934f3a1,
+    0x83264aa55c1cfac6,
+    0x1accd49ca2a491ae,
+    0xa28b2dce9e80e9a6,
+    0x34d313ea126f7c08,
+    0x0161de1ee3625456
+};
+
+uint64_t R2[6] = {
+    0x30832a73a1b25004,
+    0xa404dcf0bcb14011,
+    0xb7520b89a32a1bcc,
+    0x6154e2a7cfb8d35a,
+    0x475fc7349417d690,
+    0x0155f398d8e0e30f
+};
+
+void print(uint64_t a[6]) {
+    printf("0x");
+    for (int i = 5; i >=0; i--) {
+        printf("%016lX", a[i]);
+        if(i != 0) printf("_");
     }
     printf("\n");
 }
 
-///
-
-bool is_negative(uint32_t a[12]) {
-    return a[11] & 0x80000000;
-}
-
-bool is_zero(uint32_t a[12]) {
-    for (int i = 0; i < 12; i++) {
-        if (a[i] != 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
-bool geq(uint32_t a[12], uint32_t b[12]) {
-    bool sign_a = is_negative(a);
-    bool sign_b = is_negative(b);
-    if (sign_a != sign_b) {
-        return !sign_a;
-    }
-    for (int i = 11; i >= 0; i--) {
+bool geq(uint64_t a[6], uint64_t b[6]) {
+    for (int i = 5; i >= 0; i--) {
         if (a[i] != b[i]) {
-            return sign_a ? a[i] < b[i] : a[i] > b[i];
+            return a[i] > b[i];
         }
     }
     return true;
 }
 
-bool eq(uint32_t a[12], uint32_t b[12]) {
-    for (int i = 11; i >= 0; i--) {
+bool eq(uint64_t a[6], uint32_t b[6]) {
+    for (int i = 5; i >= 0; i--) {
         if (a[i] != b[i]) {
             return false;
         }
@@ -50,95 +67,121 @@ bool eq(uint32_t a[12], uint32_t b[12]) {
     return true;
 }
 
-int lzeroes(uint32_t a[12]) {
-    int zeroes = 0;
-    for(int i = 11; i >= 0; i--) {
-        if(a[i]==0) zeroes += 32;
-        else {
-            zeroes += __builtin_clzll(a[i]);
-            break;
-        }
-    }
-    return zeroes;
+void copy(uint64_t res[6], uint64_t a[6]){
+    for(int i=0; i<6; i++) res[i] = a[i];
 }
 
-int degree(uint32_t a[12]) {
-    return 384 - lzeroes(a); //12 *32
-}
-
-///
-
-void negate(uint32_t res[12], uint32_t a[12]){
-    for(int i = 0; i < 12; i++) {
-        res[i] = ~a[i];
-    }
-    for(int i = 0; i < 12; i++) {
-        if(res[i] == UINT64_MAX) {
-            res[i] == 0;
-        }
-        else {
-            res[i]++;
-            break;
-        }
-    }
-}
-
-void lshift(uint32_t res[12], uint32_t a[12], int shift){
-    if(shift == 0){
-        memcpy(res, a, 12*sizeof(uint32_t));
-        return;
-    }
-    int bit_shift = shift % 32;
-    int word_shift = shift / 32;
-    for (int i = 11; i >= 0; i--) {
-        res[i] = (a[i] << shift);
-        if (i > 0) res[i] |= (a[i-1] >> (32-shift));
-    }
-    for (int i = 11; i >= 0; i--){
-        if(i - word_shift >= 0) res[i] = res[i - word_shift];
-        else res[i] = 0;
-    }
-}
-
-void rshift(uint32_t res[12], uint32_t a[12], int shift){
-    int bit_shift = shift % 32;
-    int word_shift = shift / 32;
-    for (int i = 0; i < 12; i++) {
+void rshift(uint64_t res[6], uint64_t a[6], int shift){
+    int bit_shift = shift % 64;
+    int word_shift = shift / 64;
+    for (int i = 0; i < 6; i++) {
         res[i] = (a[i] >> shift);
-        if (i < 11) res[i] |= (a[i+1] << (32-shift));
+        if (i < 5) res[i] |= (a[i+1] << (64-shift));
     }
-    for (int i = 0; i < 12; i++){
-        if(i + word_shift < 12) res[i] = res[i + word_shift];
+    for (int i = 0; i < 6; i++){
+        if(i + word_shift < 6) res[i] = res[i + word_shift];
         else res[i] = 0;
     }
 }
 
-bool add(uint32_t res[12], uint32_t a[12], uint32_t b[12]){
+bool add(uint64_t res[6], uint64_t a[6], uint64_t b[6]) {
     uint64_t carry = 0;
-    for (int i = 0; i < 12; i++) {
-        uint64_t sum = (uint64_t)a[i] + (uint64_t)b[i] + carry;
-        res[i] = sum & 0xFFFFFFFF;
-        carry = sum >> 32;
+    for (int i = 0; i < 6; i++) {
+        uint64_t ai = a[i];
+        uint64_t sum = a[i] + b[i] + carry;
+        res[i] = sum;
+        carry = sum < ai;
     }
     return carry;
 }
 
-bool sub(uint32_t res[12], uint32_t a[12], uint32_t b[12]){
+bool sub(uint64_t res[6], uint64_t a[6], uint64_t b[6]) {
     uint64_t carry = 0;
-    for (int i = 0; i < 12; i++) {
-        uint64_t sum = (uint64_t)a[i] - (uint64_t)b[i] - carry;
-        res[i] = sum & 0xFFFFFFFF;
-        carry = sum >> 32;
+    for (int i = 0; i < 6; i++) {
+        uint64_t ai = a[i];
+        uint64_t sum = a[i] - b[i] - carry;
+        res[i] = sum;
+        carry = sum > ai;
     }
     return carry;
 }
 
+void addMod(uint64_t res[6], uint64_t a[6], uint64_t b[6]) {
+    add(res, a, b);
+    if(geq(res, P)) {
+        sub(res, res, P);
+    }
+}
+
+void subMod(uint64_t res[6], uint64_t a[6], uint64_t b[6]) {
+    if(geq(a, b)) {
+        printf("aaa\n");
+        sub(res, a, b);
+    } else {
+        printf("bbb\n");
+        add(res, a, P);
+        sub(res, res, b);
+    }
+}
+
+void prodMod(uint64_t res[6], uint64_t a[6], uint64_t b[6]){
+    memset(res, 0, 6*sizeof(uint64_t));
+    for(int i=376; i>=0; i--){
+        int bit = i % 64;
+        int word = i / 64;
+        addMod(res, res, res);
+        if(a[word] >> bit & 1){
+            addMod(res, res, b);
+        }
+    }
+}
+
+void prodMon(uint64_t res[6], uint64_t a[6], uint64_t b[6]){
+    memset(res, 0, 6*sizeof(uint64_t));
+    int a0 = a[0] & 1;
+    for(int i=0; i < 377; i++){
+        int bit = i % 64;
+        int word = i / 64;
+        int qa = b[word] >> bit & 1;
+        int qm = (res[0] & 1) ^ (a0 & qa);
+        if(qa){
+            add(res, res, a);
+        }
+        if(qm){
+            add(res, res, P);
+        }
+        rshift(res, res, 1);
+    }
+    if(geq(res, P)){
+        sub(res, res, P);
+    }
+}
 
 int main(){
-    uint32_t a[12] = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0x80000000}; // -11
-    uint32_t b[12] = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0x80000000}; // -1
-    uint32_t c[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // 0
-    
-    add(c, a, b);
-    print(c);
+    uint64_t a[6] = {
+        0x6363e703c89167cc,
+        0x83368d7243d7e492,
+        0x42ee81675b84219b,
+        0x49a9193f7d21d636,
+        0xaf5a0dfc4f53d7bc,
+        0x128180471539142,
+    };
+    uint64_t b[6] = {
+        0x5bf1af2de2eb371d,
+        0x5f4083ef8f614993,
+        0x2eb35f8a0cb06c48,
+        0x93469109d71ea0e7,
+        0x71b544c329b5a0bf,
+        0x11db9d324ccc864,
+    };
+
+    uint64_t res[6];
+
+    for(int i = 0; i < 1000000; i++){
+        prodMon(res, a, b);
+        copy(b,a);
+        copy(a,res);
+    }
+    print(a);
+    print(b);
 }
